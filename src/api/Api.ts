3f6386edc9e8b66b5b468f434a3f2232562bc4ca
Marcinwrzs <marcin.wrzs@gmail.com>
2025-08-03 13:04:34 +0200

    installed dependencies and api

---
+++ src/api/Api.ts
@@ -0,0 +1,19 @@
+import apiClient from "./client";
+import type { ProductListResponse } from "../types/types";
+
+class Api {
+  async getAllProducts(): Promise<ProductListResponse> {
+    const res = await apiClient.get<ProductListResponse>("/products");
+    return res.data;
+  }
+
+  async searchProducts(query: string): Promise<ProductListResponse> {
+    const res = await apiClient.get<ProductListResponse>(
+      `/products/search?q=${query}`
+    );
+    return res.data;
+  }
+}
+
+const api = new Api();
+export default api;
